900e267166ccc0b6c49ef13de273d55b17f58d91
Matin NazariBaqa <matinnazaribaqa@gmail.com>
2020-10-30 17:01:52 +0330

    added contacts app

---
+++ SinSol-project/contacts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Contact(models.Model):
+  name = models.CharField(max_length=100)
+  phone = models.CharField(max_length=100)
+  email = models.EmailField()
+  subject = models.CharField(max_length=150)
+  message = models.TextField()
+
+  def __str__(self):
+    return self.subject
+
+  
